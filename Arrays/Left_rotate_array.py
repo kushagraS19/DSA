8bd940533e24206eeb41a8df8d9c3e2592dfd816
Kushagra Saini <kushagra.saini19@gmail.com>
2026-09-02 15:48:28 +0530

    left rotate array by k places

--- Arrays/Left_rotate_array.py
+++ Arrays/Left_rotate_array.py
@@ -1,3 +1,6 @@
+# Rotate array by one -->
+
+"""
 arr = [1,2,3,4,5]
 l = 0
 r = 1
@@ -8,3 +11,21 @@ for i in range(len(arr)-1):
     l += 1
 
 print(arr)
+
+"""
+
+# Rotate array by k places -->
+
+def rotate_array (nums : list[int], k : int):
+    n = len(nums)
+    if k >= n:
+        k = k % n
+
+    nums.reverse()
+
+    nums[:k] = reversed(nums[:k])
+    nums[k:] = reversed(nums[k:])
+
+nums = [1,2,3,4,5,6]
+rotate_array(nums, 6)
+print(nums)
